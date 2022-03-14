1208e54108103a4c817498f7c95af535d774a6fe
MorgalNikita <78976288+MorgalNikita@users.noreply.github.com>
2022-03-15 02:27:24 +0300

    CodeStyle fix and SystemVerilog translate

--- tb/tb.sv
+++ tb/tb.sv
@@ -1,9 +1,16 @@
 `timescale 1ps/1ps
+//=================================================
+// Top test module and global instance
+//=================================================
 module tb();
 
-reg 			clk;
-reg 			resetn;
+logic 	clk;
+logic 	resetn;
 
+
+//=================================================
+// Clk pulse
+//=================================================
 initial begin
 	clk = 0;
 	resetn = 0;
@@ -12,41 +19,45 @@ initial begin
 end
 always #5 clk = ~clk;
 
-wire  			master_1_req;
-wire 	[31:0] 	master_1_addr;
-wire			master_1_cmd;
-wire 	[31:0]	master_1_wdata;
-wire 			master_1_ack;
-wire	[31:0]	master_1_rdata;
 
-wire  			master_2_req;
-wire 	[31:0] 	master_2_addr;
-wire			master_2_cmd;
-wire 	[31:0]	master_2_wdata;
-wire 			master_2_ack;
-wire	[31:0]	master_2_rdata;
+logic  		master_1_req;
+logic 	[31:0] 	master_1_addr;
+logic		master_1_cmd;
+logic 	[31:0]	master_1_wdata;
+logic 		master_1_ack;
+logic	[31:0]	master_1_rdata;
 
-wire  			slave_1_req;
-wire 	[31:0] 	slave_1_addr;
-wire			slave_1_cmd;
-wire 	[31:0]	slave_1_wdata;
-wire 			slave_1_ack;
-wire	[31:0]	slave_1_rdata;
+logic  		master_2_req;
+logic 	[31:0] 	master_2_addr;
+logic		master_2_cmd;
+logic 	[31:0]	master_2_wdata;
+logic 		master_2_ack;
+logic	[31:0]	master_2_rdata;
 
-wire  			slave_2_req;
-wire 	[31:0] 	slave_2_addr;
-wire			slave_2_cmd;
-wire 	[31:0]	slave_2_wdata;
-wire 			slave_2_ack;
-wire	[31:0]	slave_2_rdata;
+logic  		slave_1_req;
+logic 	[31:0] 	slave_1_addr;
+logic		slave_1_cmd;
+logic 	[31:0]	slave_1_wdata;
+logic 		slave_1_ack;
+logic	[31:0]	slave_1_rdata;
 
+logic  		slave_2_req;
+logic 	[31:0] 	slave_2_addr;
+logic		slave_2_cmd;
+logic 	[31:0]	slave_2_wdata;
+logic 		slave_2_ack;
+logic	[31:0]	slave_2_rdata;
+
+
+//=================================================
+// Global instance to testing 2x2 cross-bar
+//=================================================
 master_cpu  #(
 	.RANDOMIZATION(5)
 )
-master_1	(
-
-	.clk			(clk			),
-	.resetn			(resetn			),
+master_1(
+	.clk		(clk		),
+	.resetn		(resetn		),
 	.master_1_req	(master_1_req	),
 	.master_1_addr	(master_1_addr	),
 	.master_1_cmd	(master_1_cmd	),
@@ -58,9 +69,9 @@ master_1	(
 master_cpu  #(
 	.RANDOMIZATION(10)
 )
-master_2	(
-	.clk			(clk			),
-	.resetn			(resetn			),
+master_2(
+	.clk		(clk		),
+	.resetn		(resetn		),
 	.master_1_req	(master_2_req	),
 	.master_1_addr	(master_2_addr	),
 	.master_1_cmd	(master_2_cmd	),
@@ -70,8 +81,8 @@ master_2	(
 );
 slave_ram slave_1
 (
-	.clk			(clk			),
-	.resetn			(resetn			),
+	.clk		(clk		),
+	.resetn		(resetn		),
 	.slave_1_req	(slave_1_req	),
 	.slave_1_addr	(slave_1_addr	),
 	.slave_1_cmd	(slave_1_cmd	),
@@ -79,10 +90,9 @@ slave_ram slave_1
 	.slave_1_ack	(slave_1_ack	),
 	.slave_1_rdata	(slave_1_rdata	)
 );
-slave_ram slave_2
-(
-	.clk			(clk			),
-	.resetn			(resetn			),
+slave_ram slave_2(
+	.clk		(clk		),
+	.resetn		(resetn		),
 	.slave_1_req	(slave_2_req	),
 	.slave_1_addr	(slave_2_addr	),
 	.slave_1_cmd	(slave_2_cmd	),
@@ -90,11 +100,9 @@ slave_ram slave_2
 	.slave_1_ack	(slave_2_ack	),
 	.slave_1_rdata	(slave_2_rdata	)
 );
-
-cross_bar cross_bar
-(
-	.clk			(clk			),
-	.resetn			(resetn			),
+cross_bar cross_bar(
+	.clk		(clk		),
+	.resetn		(resetn		),
 	.master_1_req	(master_1_req	),
 	.master_1_addr	(master_1_addr	),
 	.master_1_cmd	(master_1_cmd	),
