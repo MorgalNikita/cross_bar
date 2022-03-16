6a7b7e282c750191dfeb2745b5b5240f9d28e97b
MorgalNikita <78976288+MorgalNikita@users.noreply.github.com>
2022-03-16 03:05:18 +0300

    Global fix tb, add interface

--- tb/tb.sv
+++ tb/tb.sv
@@ -20,34 +20,14 @@ end
 always #5 clk = ~clk;
 
 
-logic  		master_1_req;
-logic 	[31:0] 	master_1_addr;
-logic		master_1_cmd;
-logic 	[31:0]	master_1_wdata;
-logic 		master_1_ack;
-logic	[31:0]	master_1_rdata;
+//=================================================
+// Interface inst
+//=================================================
+master_slave_interface     	master_1_i();
+master_slave_interface     	master_2_i();
 
-logic  		master_2_req;
-logic 	[31:0] 	master_2_addr;
-logic		master_2_cmd;
-logic 	[31:0]	master_2_wdata;
-logic 		master_2_ack;
-logic	[31:0]	master_2_rdata;
-
-logic  		slave_1_req;
-logic 	[31:0] 	slave_1_addr;
-logic		slave_1_cmd;
-logic 	[31:0]	slave_1_wdata;
-logic 		slave_1_ack;
-logic	[31:0]	slave_1_rdata;
-
-logic  		slave_2_req;
-logic 	[31:0] 	slave_2_addr;
-logic		slave_2_cmd;
-logic 	[31:0]	slave_2_wdata;
-logic 		slave_2_ack;
-logic	[31:0]	slave_2_rdata;
-
+master_slave_interface     	slave_1_i();
+master_slave_interface		slave_2_i();
 
 //=================================================
 // Global instance to testing 2x2 cross-bar
@@ -56,80 +36,44 @@ master_cpu  #(
 	.RANDOMIZATION(5)
 )
 master_1(
-	.clk		(clk		),
-	.resetn		(resetn		),
-	.master_1_req	(master_1_req	),
-	.master_1_addr	(master_1_addr	),
-	.master_1_cmd	(master_1_cmd	),
-	.master_1_wdata	(master_1_wdata	),
-	.master_1_ack	(master_1_ack	),
-	.master_1_rdata	(master_1_rdata	)
+	.clk		(clk				),
+	.resetn		(resetn				),
+	.master		(master_1_i.master_interface	)
 );
 
 master_cpu  #(
 	.RANDOMIZATION(10)
 )
 master_2(
-	.clk		(clk		),
-	.resetn		(resetn		),
-	.master_1_req	(master_2_req	),
-	.master_1_addr	(master_2_addr	),
-	.master_1_cmd	(master_2_cmd	),
-	.master_1_wdata	(master_2_wdata	),
-	.master_1_ack	(master_2_ack	),
-	.master_1_rdata	(master_2_rdata	)
+	.clk		(clk				),
+	.resetn		(resetn				),
+	.master		(master_2_i.master_interface	)
 );
+
+
 slave_ram slave_1
 (
-	.clk		(clk		),
-	.resetn		(resetn		),
-	.slave_1_req	(slave_1_req	),
-	.slave_1_addr	(slave_1_addr	),
-	.slave_1_cmd	(slave_1_cmd	),
-	.slave_1_wdata	(slave_1_wdata	),
-	.slave_1_ack	(slave_1_ack	),
-	.slave_1_rdata	(slave_1_rdata	)
+	.clk		(clk				),
+	.resetn		(resetn				),
+	.slave		(slave_1_i.slave_interface	)
 );
-slave_ram slave_2(
-	.clk		(clk		),
-	.resetn		(resetn		),
-	.slave_1_req	(slave_2_req	),
-	.slave_1_addr	(slave_2_addr	),
-	.slave_1_cmd	(slave_2_cmd	),
-	.slave_1_wdata	(slave_2_wdata	),
-	.slave_1_ack	(slave_2_ack	),
-	.slave_1_rdata	(slave_2_rdata	)
+
+slave_ram slave_2
+(
+	.clk		(clk				),
+	.resetn		(resetn				),
+	.slave		(slave_2_i.slave_interface	)
 );
+
+
 cross_bar cross_bar(
-	.clk		(clk		),
-	.resetn		(resetn		),
-	.master_1_req	(master_1_req	),
-	.master_1_addr	(master_1_addr	),
-	.master_1_cmd	(master_1_cmd	),
-	.master_1_wdata	(master_1_wdata	),
-	.master_1_ack	(master_1_ack	),
-	.master_1_rdata	(master_1_rdata	),
+	.clk		(clk				),
+	.resetn		(resetn				),
 
-	.master_2_req	(master_2_req	),
-	.master_2_addr	(master_2_addr	),
-	.master_2_cmd	(master_2_cmd	),
-	.master_2_wdata	(master_2_wdata	),
-	.master_2_ack	(master_2_ack	),
-	.master_2_rdata	(master_2_rdata	),
-
-	.slave_1_req	(slave_1_req	),
-	.slave_1_addr	(slave_1_addr	),
-	.slave_1_cmd	(slave_1_cmd	),
-	.slave_1_wdata	(slave_1_wdata	),
-	.slave_1_ack	(slave_1_ack	),
-	.slave_1_rdata	(slave_1_rdata	),
-
-	.slave_2_req	(slave_2_req	),
-	.slave_2_addr	(slave_2_addr	),
-	.slave_2_cmd	(slave_2_cmd	),
-	.slave_2_wdata	(slave_2_wdata	),
-	.slave_2_ack	(slave_2_ack	),
-	.slave_2_rdata	(slave_2_rdata	)
+	.master_1	(master_1_i.master_interface	),
+	.master_2	(master_2_i.master_interface	),
+	.slave_1	(slave_1_i.slave_interface	),
+	.slave_2	(slave_2_i.slave_interface	)
 );
 
 endmodule
